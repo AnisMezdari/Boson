78ee1bd0c6478fc90fa0cf284971e84a721fd753
Anis Mezdari <mezdari.anis@gmail.com>
2019-04-24 18:12:35 +0200

    amelioration menu / back to menu

---
+++ Assets/BackToMenu.cs
@@ -0,0 +1,29 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviourPun
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnclickExitButtonu(){
+
+        PhotonNetwork.LeaveRoom();
+        PhotonNetwork.Disconnect();
+        SceneManager.LoadScene("Menu");
+       
+    }
+}
